ca9b8a6ff83154dcb4707d04324afcf37bd5b03b
BWizard06 <89217401+BWizard06@users.noreply.github.com>
2022-10-05 22:34:00 +0200

    Grundgerüst und erste Task einfügen

--- src/script.js
+++ src/script.js
@@ -1,15 +1,37 @@
+const createCell = (cellText) => {
+    const cell = document.createElement("td");
+    cell.innerText = cellText;
+    return cell;
+}
 
+const addTask =(task) => {
+    const task_list = document.getElementById("task-list");
+    const tableRow = document.createElement("tr");
 
+    tableRow.append(
+        createCell(task.id), 
+        createCell(task.completed), 
+        createCell(task.title)
+    );
+    
+    task_list.appendChild(tableRow);
+}
+
+const tasks_url = 'http://localhost:3000/tasks';
 let tasksData;
-const getTasks = () => {
-    fetch('http://localhost:3000/tasks')
-        .then(response => response.json()) 
-        .then((data) => {
-            return data;
-        });
-};
+fetch(tasks_url)
+    .then((response) =>{
+        return response.json();
+    })
+    .then((data) => {
+        addTask(data[0]);
+        //Lehrer fragen weil ich nur auf einen expliziten Eintrag zugreifen kann
+    });
 
-document.addEventListener('DOMContentLoaded', () => {
-    tasksData = getTasks();
-    console.log(tasksData);
-});
+
+
+
+
+
+
+    
