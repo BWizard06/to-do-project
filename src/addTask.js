d2046f6889823cd104d12849c35108db18e2bb3d
BWizard06 <89217401+BWizard06@users.noreply.github.com>
2022-10-06 13:46:33 +0200

    able to delete and change tasks

--- src/addTask.js
+++ src/addTask.js
@@ -1,15 +1,8 @@
 const tasks_url = 'http://localhost:3000/tasks';
-
-
 document.addEventListener("DOMContentLoaded", () => {
-    const taskName = document.getElementById("taskName");
     function createTask (task) {
-        fetch(tasks_url, {method: `POST`, headers: {'Content-Type': 'application/json'}, body: JSON.stringify(task)})
-        
+        fetch(tasks_url, {method: `POST`, headers: {'Content-Type': 'application/json'}, body: JSON.stringify(task)})   
     }
-    
-
-
     function logSubmit(event) {
         log.textContent = `Task Submitted!`;
         event.preventDefault();
@@ -18,14 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
       const form = document.getElementById('addTaskForm');
       const log = document.getElementById('log');
       form.addEventListener('submit', ()=> {
-        
-        
-        
-        
+        const taskName = document.getElementById("taskName");
         let task = {
             title: taskName.value,
         }
         createTask(task);
-
       }); 
     })
--- src/index.js
+++ src/index.js
@@ -4,15 +4,43 @@ const createCell = (cellText) => {
     return cell;
 }
 
+function changeTask(task){
+    task.completed = !task.completed;
+    fetch(`http://localhost:3000/tasks`, {
+        method: `PUT`,headers: {'Content-Type': 'application/json'}, body: JSON.stringify(task)
+    })
+
+}
+
+function deleteTask(id){
+    fetch(`http://localhost:3000/task/${id}`, {method: `DELETE`})
+    .then(window.location.reload())
+}
+
 function getTasks(tasks) {
     const task_list = document.getElementById("task-list");
     tasks.forEach((task) => {
         const tableRow = document.createElement("tr");
+        const deleteButton = document.createElement("button");
+        const checkBox = document.createElement("input");
+        const chckcell = document.createElement("td");
+
         tableRow.append(
             createCell(task.id), 
             createCell(task.title), 
-            createCell(task.completed)
         );
+        deleteButton.innerText = "Delete";
+        chckcell.appendChild(checkBox);
+        tableRow.appendChild(chckcell);
+        tableRow.appendChild(deleteButton);
+        deleteButton.classList.add("btn", "btn-danger", "btn-sm", "delete-button",);
+        deleteButton.setAttribute("id","delete-button");
+        deleteButton.addEventListener("click", () => deleteTask(task.id));
+        checkBox.addEventListener("change", () => changeTask(task));
+        checkBox.setAttribute("type", "checkbox");
+        checkBox.checked = task.completed;
+
+        
         
         task_list.appendChild(tableRow);
     });
@@ -29,13 +57,4 @@ function indexTasks(){
 
 document.addEventListener("DOMContentLoaded", () => {
     indexTasks();
-})
-
-
-
-
-
-
-
-
-    
+});
