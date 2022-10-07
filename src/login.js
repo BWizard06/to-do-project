7d673a9257d0cdd2443d76be4d6472a51f56abb0
BWizard06 <89217401+BWizard06@users.noreply.github.com>
2022-10-07 14:25:03 +0200

    remove src folder from the linkings

--- src/login.js
+++ src/login.js
@@ -1,47 +1,43 @@
 let email = document.getElementById("email");
-let password = document.getElementById("password"); 
-const login_url = "http://localhost:3000/auth/jwt/sign"
+let password = document.getElementById("password");
+const login_url = "http://localhost:3000/auth/jwt/sign";
 document.addEventListener("DOMContentLoaded", () => {
-    function createLogin(login){
-        fetch(login_url, {
-            method: `POST`, 
-            headers: {'Content-Type': 'application/json'}, 
-            body: JSON.stringify(login)})
-            .then((response) => {
-                
-                if (response.status < 300) {
-                    window.location.href = "./index.html";
-                    return response.json();
-                } else{
-                    const loginTitle = document.getElementById("loginTitle");
-                    loginTitle.innerText = "Wrong password";
-                    loginTitle.style.color = "red";
-                }
-            })
-
-            .then((data) => {
-                localStorage.setItem('token', data.token);
-            })
-    }
-    let form = document.getElementById("loginForm");
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        let email = document.getElementById("email");
-        let password = document.getElementById("password"); 
-        let login = {
-            email: email.value,
-            password: password.value
-        }
-        createLogin(login);
-
+  function createLogin(login) {
+    fetch(login_url, {
+      method: `POST`,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(login),
     })
-    const token = localStorage.getItem('token');
-    if (token) {
+      .then((response) => {
+        if (response.status < 300) {
+          window.location.href = "index.html";
+          return response.json();
+        } else {
+          const loginTitle = document.getElementById("loginTitle");
+          loginTitle.innerText = "Wrong password";
+          loginTitle.style.color = "red";
+        }
+      })
+
+      .then((data) => {
+        localStorage.setItem("token", data.token);
+      });
+  }
+  let form = document.getElementById("loginForm");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let login = {
+      email: email.value,
+      password: password.value,
+    };
+    createLogin(login);
+  });
+  const token = localStorage.getItem("token");
+  if (token) {
     loginTitle.style.color = "green";
     loginTitle.innerText = "You are already logged in";
     form.hidden = true;
-}
-})
-
-
-
+  }
+});
